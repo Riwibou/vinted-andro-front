abe9f2ea532fd113ebbeec0120272c1598c0711f
Riwan William Bououli <riwan.b@live.fr>
2024-03-30 19:11:19 +0100

    css card work in progress ++

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,27 @@
-
+import { Link } from "react-router-dom";
 
 const Card = ({offer}) => {
-  const {_id, owner, product_image, product_details, product_price} = offer;
+
+  const {_id, owner, product_image, product_details, product_price, product_name} = offer;
+
   return (
     <>
     <div className="card" key={_id}>
       <div className="card-header">
-        <h3 className="text-muted">Card header</h3>
+        <img src={owner.account.avatar?.secure_url} alt="" />
+        <h3 className="text-muted">{owner.account.username}</h3>
       </div>
 
       <div className="card-body">
-        <img src="" alt="" />
+        <Link to={`/offer/${_id}`}>
+          <img src={product_image.secure_url} alt={product_name} />
+        </Link>
       </div>
 
       <div className="card-footer">
-        <p>2 days ago</p>
-        <p className="text-muted">blabla</p>
+        <p className="text-muted">{product_price} €</p>
+        <p className="text-muted">{product_details[1].TAILLE}</p>
+        <p className="text-muted">{product_details[0].MARQUE}</p>
       </div>
     </div>
   </>
